2ab3e02653f2840cc3238ebcca66321997900d0f
tinho1123 <tinhoefaela@gmail.com>
2026-04-11 14:03:55 -0300

    space

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -124,7 +124,7 @@ class UserResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index'  => Pages\ListUsers::route('/'),
+            'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
         ];
     }
